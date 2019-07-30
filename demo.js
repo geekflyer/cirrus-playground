1884d5ffba384e78c81888d1969e9dade927886d
Christian Theilemann <christian@solvvy.com>
2019-07-30 12:18:33 -0700

    add cirrus yml

---
+++ demo.js
@@ -0,0 +1,11 @@
+const ONE_SECOND = 1000;
+
+let counter = 0;
+const countUntil = 20
+
+const intervalTimer = setInterval(() => {
+  console.log(++counter);
+  if (counter === countUntil) {
+    clearInterval(intervalTimer);
+  }
+}, ONE_SECOND);
